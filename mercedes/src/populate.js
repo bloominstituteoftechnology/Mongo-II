946ccd6c20458da27084b8ab4d6c4d56e92cb0b6
mixelpixel <mixelpix@mac.com>
2017-08-17 00:44:36 -0400

    setup for pair programming

---
+++ mercedes/src/populate.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+
+let savedPosts = null;
+
+const readPosts = () => {
+  // cache posts after reading them once
+  if (!savedPosts) {
+    const contents = fs.readFileSync('posts.json', 'utf8');
+    savedPosts = JSON.parse(contents);
+  }
+  return savedPosts;
+};
+
+const populatePosts = () => {
+  // TODO: implement this
+};
+
+module.exports = { readPosts, populatePosts };
---
+++ mercedes/src/post.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+// Clear out mongoose's model cache to allow --watch to work for tests:
+// https://github.com/Automattic/mongoose/issues/1251
+mongoose.models = {};
+mongoose.modelSchemas = {};
+
+mongoose.Promise = Promise;
+mongoose.connect('mongodb://localhost/so-posts', { useMongoClient: true });
+
+const PostSchema = new mongoose.Schema({
+  // TODO: write your schema here
+});
+
+module.exports = mongoose.model('Posts', PostSchema);
