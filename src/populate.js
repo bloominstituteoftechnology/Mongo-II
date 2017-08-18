26906d05010563c4bad64e55ba087bc2277477fc
WizrdJim <wzrdjim@gmail.com>
2017-08-17 20:44:42 -0400

    here jesh todays work

--- src/populate.js
+++ src/populate.js
@@ -1,6 +1,13 @@
 const fs = require('fs');
+const Post = require('./post.js');
+const mongoose = require('mongoose');
 
 let savedPosts = null;
+// mongoose.Promise = global.Promise;
+// mongoose.connect(
+//   'mongodb://localhost/so-posts',
+//   { useMongoClient: true }
+// );
 
 const readPosts = () => {
   // cache posts after reading them once
@@ -13,6 +20,13 @@ const readPosts = () => {
 
 const populatePosts = () => {
   // TODO: implement this
+  readPosts();
+  const promises = savedPosts.map(p => new Post(p).save());
+  return Promise.all(promises);
 };
+populatePosts().then((allPosts) => {
+  return allPosts;
+});
+
 
 module.exports = { readPosts, populatePosts };
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const Post = require('./post');
 
 const STATUS_USER_ERROR = 422;
 
@@ -8,5 +9,37 @@ const server = express();
 server.use(bodyParser.json());
 
 // TODO: write your route handlers here
+
+server.get('/accepted-answer/:soID', (req, res) => {
+  const { soID } = req.params;
+  let acceptedAnswerID = 0;
+  Post.find({ soID }, (error, data) => {
+    if (error) {
+      res.status(500);
+      res.json({ error });
+    } else {
+    }
+  });
+  console.log(acceptedAnswerID);
+  Post.find({ soID: acceptedAnswerID }, (error, data) => {
+    if (error) {
+      res.status(500);
+      res.json({ error });
+    }
+    res.json({ data });
+  });
+});
+
+server.get('/top-answer/:soID', (req, res) => {
+  const { soID } = req.params;
+});
+
+server.get('/popular-jquery-questions', (req, res) => {
+
+});
+
+server.get('/npm-answers', (req, res) => {
+
+});
 
 module.exports = { server };
