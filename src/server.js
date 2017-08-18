7da7122069c70beca2b601a4397ee1d2e9b0e43c
jeshsy <jeshsy@users.noreply.github.com>
2017-08-18 15:29:33 -0400

    2 tests fail

--- src/server.js
+++ src/server.js
@@ -13,42 +13,113 @@ server.use(bodyParser.json());
 const sendUserError = (err, res) => {
   res.status(STATUS_USER_ERROR);
   if (err === 'string') {
-    res.json({ error: err })
+    res.json({ error: err });
   } else {
-    res.json(err)
-  };
+    res.json(err);
+  }
 };
 
-server.get('/accepted-answer/:soID', (req, res) => {
-  const { soID } = req.params;
-  let acceptedAnswerID = 0;
-  Post.find({ soID }, (error, data) => {
+server.get('/', (req, res) => {
+  Post.find({}, (error, data) => {
     if (error) {
       res.status(500);
       res.json({ error });
     } else {
+      res.json(data);
     }
   });
-  console.log(acceptedAnswerID);
-  Post.find({ soID: acceptedAnswerID }, (error, data) => {
-    if (error) {
-      res.status(500);
-      res.json({ error });
-    }
-    res.json({ data });
-  });
+});
+
+// server.get('/accepted-answer/:soID', (req, res) => {
+//   const { soID } = req.params;
+//   const acceptedAnswerID = 0;
+//   Post.find({ soID }, (error, data) => {
+//     if (error) {
+//       res.status(500);
+//       res.json({ error });
+//     }
+//   });
+//   console.log(acceptedAnswerID);
+//   Post.find({ soID: acceptedAnswerID }, (error, data) => {
+//     if (error) {
+//       res.status(500);
+//       res.json({ error });
+//     }
+//     res.json({ data });
+//   });
+// });
+
+server.get('/accepted-answer/:soID', (req, res) => {
+  const { soID } = req.params;
+  Post.findOne({ soID })
+    .exec((err, post) => {
+      if (!post) {
+        sendUserError(err, res);
+        return;
+      }
+      Post.findOne({ soID: post.acceptedAnswerID })
+        .exec((error, answer) => {
+          if (!answer) {
+            sendUserError(error, res);
+            return;
+          }
+          res.json(answer);
+        });
+    });
 });
 
 server.get('/top-answer/:soID', (req, res) => {
   const { soID } = req.params;
+  Post.findOne({ soID })
+  .exec((error, post) => {
+    if (!post) {
+      sendUserError(error, res);
+      return;
+    }
+    Post.find({ parentID: soID }, { soID: { $ne: post.acceptedAnswerID } })
+    .sort({ score: -1 })
+    .limit(1)
+    .exec((err, answer) => {
+      if (!answer) {
+        sendUserError(err, res);
+        return;
+      }
+      res.json(answer);
+    });
+  });
 });
 
 server.get('/popular-jquery-questions', (req, res) => {
-
+  Post.find({ $and: [{ tags: 'jquery' }, { $or: [{ score: { $gt: 5000 } }, { 'user.reputation': { $gt: 200000 } }] }] })
+  .exec((error, questions) => {
+    if (!questions) {
+      sendUserError(error, res);
+      return;
+    }
+    res.json(questions);
+  });
 });
 
 server.get('/npm-answers', (req, res) => {
-
+  const idArr = [];
+  Post.find({ tags: 'npm' }, { soID: 1, _id: 0 })
+  .exec((err, questions) => {
+    if (!questions) {
+      sendUserError(err, res);
+      return;
+    }
+    console.log(questions);
+    // for loop over the array to make a new array of just the ids
+    // then you can do post.find(for(loop over array for IDs) do `or` operation)
+    Post.find({ parentID: { $or: questions } })
+    .exec((error, answers) => {
+      if (!answers) {
+        sendUserError(error, res);
+        return;
+      }
+      res.json(answers);
+    });
+  });
 });
 
 module.exports = { server };
