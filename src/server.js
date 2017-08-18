efa5ade617d52d97d5111ce7de0798cf156e9fea
wlharvey4 <pinecone062@gmail.com>
2017-08-18 09:52:54 -0700

    fixed 2 errors in server.js; passing tests

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const bodyParser = require('body-parser');
 const express = require('express');
 
 const STATUS_USER_ERROR = 422;
+const Post = require('./post.js');
 
 const server = express();
 // to enable parsing of json bodies for post requests
@@ -13,13 +14,15 @@ server.get('/accepted-answer/:soID', (req, res) => {
   Post.findOne({ soID })
     .exec((err, post) => {
       if (!post) {
-        sendUserError(err, res);
+        res.status(STATUS_USER_ERROR);
+        res.json({ error: 'User Error in accepted-answer' });
         return;
       }
       Post.findOne({ soID: post.acceptedAnswerID })
         .exec((error, answer) => {
           if (!answer) {
-            sendUserError(error, res);
+            res.status(STATUS_USER_ERROR);
+            res.json({ error: 'User Error in accepted-answer findOne' });
             return;
           }
           res.json(answer);
