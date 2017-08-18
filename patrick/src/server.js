05ae032771a0cd47790b50baa2232e2c7cbb4b84
mixelpixel <mixelpix@mac.com>
2017-08-18 00:29:44 -0400

    5/9 passing √

--- patrick/src/server.js
+++ patrick/src/server.js
@@ -39,5 +39,38 @@ server.get('/accepted-answer/:soID', (req, res) => {
     });
 });
 
+server.get('/top-answer/:soID', (req, res) => {
+  const { soID } = req.params;
+  // res.json(soID);
+  Post.findOne({ soID }).exec((err, post) => {
+    if (!post) {
+      sendUserError(err, res);
+      return;
+    }
+    // Post.findOne({ soID: post.acceptedAnswerID }).exec((error, answer) => {
+    const bob = post.acceptedAnswerID;
+    const score = post.score;
+    Post.find({ $and: [{ bob: { $eq: 'null' } }, { score: { $eq: 11159 } }] }).exec((error, answer) => {
+      if (!answer) {
+        sendUserError(error, res);
+        return;
+      }
+      res.json(answer);
+    });
+  });
+});
+
+// ### `GET /top-answer/:soID`
+// When the client makes a `GET` request to `/top-answer/:soID`:
+//
+// 1. Find the question with the given `soID` (1 query).
+// 2. Find the answer of the given question that has the *highest* score and *is
+//    not the accepted answer* (1 query).
+// 3. Send back a JSON response with the single top answer post object.
+//
+// You should *only* use 2 queries, and no more, for this route. If there's any
+// error, or if there is no top answer, report it with an appropriate message
+// and status code.
+
 
 module.exports = { server };
