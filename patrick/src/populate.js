9ceb89c1fc4849df16c4d180b396417e7d14ce1d
mixelpixel <mixelpix@mac.com>
2017-08-17 23:16:53 -0400

    populate √

--- patrick/src/populate.js
+++ patrick/src/populate.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const Post = require('./post');
+
 const posts = require('../posts.json');
 const mongoose = require('mongoose');
 
@@ -14,39 +15,12 @@ const readPosts = () => {
   return savedPosts;
 };
 
-// mongoose.Promise = global.Promise;
-// mongoose.connect(
-//   'mongodb://localhost/posts',
-//   { useMongoClient: true }
-// );
-
 const populatePosts = () => {
   // TODO: implement this
-  // savedPosts = readPosts();
   readPosts();
   const promises = savedPosts.map(post => new Post(post).save());
   return Promise.all(promises);
-  // const populateSOPosts = () => {
-  //   const allPosts = posts;
-  //   const promises = allPosts.map(p => new Post(p).save());
-  //   return Promise.all(promises);
-  // };
-  // /* eslint no-console: 0 */
-  // return populateSOPosts()
-  //   .then(() => {
-  //     console.log('done');
-  //     mongoose.disconnect();
-  //   })
-  //   .catch((err) => {
-  //     console.log('ERROR', err);
-  //     throw new Error(err);
-  //   });
+  // return Promise.all(readPosts().map(p => new Post(p).save())); // <~~~ Does the same thing as above
 };
-
-// populatePosts();
-populatePosts().then((allPosts) => {
-  // console.log('DUN and DONE!!!!!!');
-  return allPosts;
-});
 
 module.exports = { readPosts, populatePosts };
