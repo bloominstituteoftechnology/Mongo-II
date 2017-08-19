c4e453357700c020aa8268e4754ec9461a288c8b
Ely Alamillo <el0013@gmail.com>
2017-08-18 22:34:12 -0500

    finished sprint

--- src/server.js
+++ src/server.js
@@ -1,4 +1,3 @@
-/* eslint-disable */
 const bodyParser = require('body-parser');
 const express = require('express');
 const Post = require('./post.js');
@@ -22,55 +21,85 @@ server.get('/posts', (req, res) => {
 
 server.get('/accepted-answer/:soID', (req, res) => {
   const id = req.params.soID;
-  Post.findOne({ soID: id })
-    .exec((err, answer) => {
-      if (err) {
+  Post.findOne({ soID: id }).exec((err, answer) => {
+    if (err) {
+      res.status(STATUS_USER_ERROR);
+      res.json(err);
+      return;
+    }
+    Post.findOne({ soID: answer.acceptedAnswerID }).exec((error, ans) => {
+      if (error) {
+        res.status(STATUS_SERVER_ERROR);
+        res.json(error);
+        return;
+      } else if (!ans) {
         res.status(STATUS_USER_ERROR);
-        res.json(err);
+        res.json(error);
         return;
       }
-      Post.findOne({ soID: answer.acceptedAnswerID })
-        .exec((error, ans) => {
-          if (error) {
-            res.status(STATUS_SERVER_ERROR);
-            res.json(error);
-            return;
-          } else if (!ans) {
-            res.status(STATUS_USER_ERROR);
-            res.json(error);
-            return;
-          }
-          res.json(ans);
-        });
+      res.json(ans);
     });
+  });
 });
 
 server.get('/top-answer/:soID', (req, res) => {
   const id = req.params.soID;
   const highestAnswer = req.params.soID.acceptedAnswerID;
-  Post.findOne({ soID: id })
-    .exec((err, answer) => {
-      if (err) {
+  Post.findOne({ soID: id }).exec((err, answer) => {
+    if (!answer) {
+      res.status(STATUS_USER_ERROR);
+      res.json({ error: 'ID was not found in the database' });
+      return;
+    }
+    Post.find({ parentID: id, soID: { $ne: answer.acceptedAnswerID } })
+      .sort({ score: 'desc' })
+      .exec((error, ans) => {
+        if (ans.length < 1) {
+          res.status(STATUS_USER_ERROR);
+          res.json({ error: 'no top answer.' });
+        }
+        res.json(ans[0]);
+      });
+  });
+});
+
+server.get('/popular-jquery-questions', (req, res) => {
+  Post.find({
+    $and: [
+      { tags: 'jquery' },
+      {
+        $or: [{ score: { $gt: 5000 } }, { 'user.reputation': { $gt: 200000 } }],
+      },
+    ],
+  }).exec((error, answer) => {
+    if (!answer) {
+      res.status(STATUS_USER_ERROR);
+      res.json(error);
+      return;
+    }
+    res.json(answer);
+  });
+});
+
+server.get('/npm-answers', (req, res) => {
+  const parentIDS = [];
+
+  Post.find({ tags: 'npm' }).exec((err, answer) => {
+    if (!answer) {
+      res.status(STATUS_USER_ERROR);
+      res.json(err);
+      return;
+    }
+    answer.forEach(question => parentIDS.push({ parentID: question.soID }));
+    // [ {parentID: }, {parentID:}]
+    // res.json(answer);
+    Post.find({ $or: parentIDS }).exec((error, ans) => {
+      if (!ans) {
         res.status(STATUS_USER_ERROR);
-        res.json({error: 'HELLO!'});
-        return;
-      } else if (!answer) {
-        res.status(STATUS_USER_ERROR);
-        res.json({ error: 'ID was not found in the database' });
+        res.json(error);
         return;
       }
-      Post.find({ parentID: id, soID: { $ne: answer.acceptedAnswerID} })
-      .sort({score: 'desc'})
-      .exec((error, ans) => {
-        if (error) {
-          res.status(STATUS_SERVER_ERROR);
-          res.json(error);
-          return;
-        } else if (ans.length < 1) {
-          res.status(STATUS_USER_ERROR);
-          res.json({error: 'no top answer.'});
-        }
-        res.json(ans[0]);  
+      res.json(ans);
     });
   });
 });
