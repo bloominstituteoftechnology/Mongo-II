7112cbeb8f326274706da594ae4fadcdfd96b224
mixelpixel <mixelpix@mac.com>
2017-08-21 11:49:26 -0400

    accepted answer (with helper method) 4/5 √

--- src/server.js
+++ src/server.js
@@ -8,5 +8,36 @@ const server = express();
 server.use(bodyParser.json());
 
 // TODO: write your route handlers here
+const Post = require('./post');
+
+// helper
+const sendUserError = (err, res) => {
+  res.status(STATUS_USER_ERROR);
+  if (typeof err === 'string') {
+    res.json({ error: err });
+  } else {
+    res.json(err);
+  }
+};
+
+server.get('/accepted-answer/:soID', (req, res) => {
+  const { soID } = req.params;
+  Post.findOne({ soID })
+    .exec((err, post) => {
+      if (!post) {
+        sendUserError(err, res);
+        return;
+      }
+      Post.findOne({ soID: post.acceptedAnswerID })
+        .exec((error, answer) => {
+          if (!answer) {
+            sendUserError(error, res);
+            return;
+          }
+          res.json(answer);
+        });
+    });
+});
+
 
 module.exports = { server };
