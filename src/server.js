893e71564fa11ad7bb4f78897b5eceb7543891b9
manishavi <manishalal972@gmail.com>
2017-08-21 11:47:38 -0500

    changes

--- src/server.js
+++ src/server.js
@@ -65,20 +65,18 @@ server.get('/popular-jquery-questions', (req, res) => {
 });
 
 server.get('/npm-answers', (req, res) => {
-  Post.find({ tags: 'npm' } , { soID: 1, _id: 0 }, (err, soids) => {
+  Post.find({ tags: 'npm' }, { soID: 1, _id: 0 }, (err, soids) => {
     if (err) {
-      sendUserError({error: 'User Error in npm-answers' }, res);
+      sendUserError({ error: 'User Error in npm-answers' }, res);
       return;
     }
     const soidsArr = soids.map(e => e.soID);
     Post.find({ parentID: { $in: soidsArr } }, (err2, answers) => {
-    .exec((errr, answer) => {
       if (err2) {
         sendUserError({ error: 'User Error in npm-answers find' });
         return;
       }
-        res.json({answers});
-      }
+      res.json({ answers });
     });
   });
 });
