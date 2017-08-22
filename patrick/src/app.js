035fc3a498b6b338d66edadc347568cac83f3691
mixelpixel <mixelpix@mac.com>
2017-08-21 20:55:55 -0400

    todo

--- patrick/src/app.js
+++ patrick/src/app.js
@@ -2,3 +2,12 @@
 const { server } = require('./server.js');
 
 server.listen(3000);
+
+/*
+So it's a bit harder with LS-Mongo-II, since the connection happens in `src/post.js`.
+We do this because we can re-use the connection across both `src/app.js` and the tests.
+To add that piece of functionality, what you could do is export out the return
+value of `mongoose.connect()` (i.e. the returned promise) from `src/post.js` alongside
+the actual `Post` model. Then you could access that promise inside `app.js` and
+call `.then()` just like in the snippet you posted.
+*/
