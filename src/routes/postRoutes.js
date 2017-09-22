9a73492c024aeb02d7c67300c3820018ba303deb
phytertek <ryan.phytertek@gmail.com>
2017-09-22 02:07:52 -0400

    refactor postRoutes

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -4,42 +4,35 @@ const resError = require('../utils/errors').resError;
 // Post methods
 const PostMethods = require('../controllers/postController');
 
+// Success method
+const sendResponse = (response, res) => !response.error
+  ? res.json(response)
+  : resError(response.error, res);
+
 const postRoutes = (server) => {
   // ### `GET /accepted-answer/:soID`
-  server.get('/accepted-answer/:soID', async (req, res) => {
-    const response = await PostMethods.getAcceptedAnswer(req.params.soID);
-    if (!response.error) {
-      return res.json(response);
-    }
-    return resError(response.error, res);
-  });
+  server.get('/accepted-answer/:soID', async (req, res) => sendResponse(
+    await PostMethods.getAcceptedAnswer(req.params.soID),
+    res
+  ));
 
   // ### `GET /top-answer/:soID`
-  server.get('/top-answer/:soID', async (req, res) => {
-    const response = await PostMethods.getTopAnswer(req.params.soID);
-    if (!response.error) {
-      return res.json(response);
-    }
-    return resError(response.error, res);
-  });
+  server.get('/top-answer/:soID', async (req, res) => sendResponse(
+    await PostMethods.getTopAnswer(req.params.soID),
+    res
+  ));
 
   // ### `GET /popular-jquery-questions`
-  server.get('/popular-jquery-questions', async (req, res) => {
-    const response = await PostMethods.getPopularJQueryQuestions();
-    if (!response.error) {
-      return res.json(response);
-    }
-    return resError(response.error, res);
-  });
+  server.get('/popular-jquery-questions', async (req, res) => sendResponse(
+    await PostMethods.getPopularJQueryQuestions(),
+    res
+  ));
 
   // ### `GET /npm-answers`
-  server.get('/npm-answers', async (req, res) => {
-    const response = await PostMethods.getNpmAnswers();
-    if (!response.error) {
-      return res.json(response);
-    }
-    return resError(response.error, res);
-  });
+  server.get('/npm-answers', async (req, res) => sendResponse(
+    await PostMethods.getNpmAnswers(),
+    res
+  ));
 };
 
 module.exports = postRoutes;
