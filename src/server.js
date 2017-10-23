278900d0bb2c644ed427310320ed49d2af1d7f9e
Nick Costigan <nick.costigan@gmail.com>
2017-10-23 12:19:07 -0400

    passes all tests

--- src/server.js
+++ src/server.js
@@ -10,6 +10,73 @@ const server = express();
 
 server.use(bodyParser.json());
 
-// TODO: write your route handlers here
+server.get('/accepted-answer/:soID', (req, res) => {
+  const { soID } = req.params;
+  Post.findOne({ soID }, 'acceptedAnswerID')
+  .then((answerID) => {
+    if (!answerID.acceptedAnswerID) {
+      res.status(STATUS_USER_ERROR).json({ error: 'post has no accepted answer' });
+      return;
+    }
+    Post.findOne({ soID: answerID.acceptedAnswerID })
+    .then((answer) => {
+      res.json(answer);
+    });
+  })
+  .catch((error) => {
+    res.status(STATUS_USER_ERROR).json({ error });
+  });
+});
+
+server.get('/top-answer/:soID', (req, res) => {
+  const { soID } = req.params;
+  Post.findOne({ soID })
+  .then((post) => {
+    Post.find({
+      soID: { $ne: post.acceptedAnswerID },
+      parentID: post.soID,
+    })
+    .sort({ score: -1 })
+    .then((posts) => {
+      if (posts.length === 0) {
+        res.status(STATUS_USER_ERROR).json({ error: 'post has no top answer' });
+        return;
+      }
+      res.json(posts[0]);
+    });
+  })
+  .catch((error) => {
+    res.status(STATUS_USER_ERROR).json({ error });
+  });
+});
+
+server.get('/popular-jquery-questions', (req, res) => {
+  Post.find({
+    tags: 'jquery',
+    $or: [{ score: { $gt: 5000 } }, { 'user.reputation': { $gt: 200000 } }]
+  })
+  .then((posts) => {
+    res.json(posts);
+  })
+  .catch((error) => {
+    res.status(STATUS_USER_ERROR).json({ error });
+  });
+});
+
+server.get('/npm-answers', (req, res) => {
+  Post.find({ tags: 'npm' })
+  .then((npmQuestions) => {
+    const ids = npmQuestions.map((post) => {
+      return post.soID;
+    });
+    Post.find({ parentID: ids })
+    .then((npmAnswers) => {
+      res.json(npmAnswers);
+    });
+  })
+  .catch((error) => {
+    res.status(STATUS_USER_ERROR).json({ error });
+  });
+});
 
 module.exports = { server };
