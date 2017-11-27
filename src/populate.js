9364f579be16b42c46d33f48dbbadff023ec21b9
Gary Dillard <thomas.dillard.studios@gmail.com>
2017-11-27 16:19:56 -0700

    server populated

--- src/populate.js
+++ src/populate.js
@@ -1,8 +1,15 @@
 const fs = require('fs');
+const mongoose = require('mongoose');
 
 let savedPosts = null;
 
 const Post = require('./post.js');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(
+  'mongodb://localhost/posts',
+  { useMongoClient: true },
+);
 
 const readPosts = () => {
   // cache posts after reading them once
@@ -15,6 +22,16 @@ const readPosts = () => {
 
 const populatePosts = () => {
   // TODO: implement this
+  const populate = () => {
+    const posts = readPosts();
+    Promise.all(posts);
+    const promises = posts.map(post => new Post(post).save());
+    return Promise.all(promises);
+  };
+  return populate()
+    .then(() => { mongoose.disconnect(), console.log('done'); })
+    .catch(err => console.log(err), mongoose.disconnect());
 };
+populatePosts();
 
 module.exports = { readPosts, populatePosts };
