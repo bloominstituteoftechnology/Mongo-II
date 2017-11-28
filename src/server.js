df8b41a176a42b37143301d914972662c56294cc
Gary Dillard <thomas.dillard.studios@gmail.com>
2017-11-27 19:23:19 -0700

    top-answer mostly working

--- src/server.js
+++ src/server.js
@@ -13,29 +13,68 @@ server.use(bodyParser.json());
 // TODO: write your route handlers here
 server.get('/accepted-answer/:soID', (req, res) => {
   const { soID } = req.params;
+  if (isNaN(soID)) res.status(422).json('bad ID');
+  // console.log(soID);
   Post.findOne({ soID })
     .select('acceptedAnswerID')
-    .exec((err, post) => {
-      if (err) res.status(STATUS_USER_ERROR);
-      else return post;
-    })
-    .then((answerID) => {
-      Post.findOne({ soID: answerID.acceptedAnswerID }, (err, post) => {
-        if (err) res.status(STATUS_USER_ERROR);
-        else return res.json(post);
+    .exec((err1, post) => {
+      // console.log('>>>>>>>>>>>>>>>>> err1', err1, post);
+      if (err1 || post === null) {
+        res.status(STATUS_USER_ERROR);
+        return;
+      }
+
+      if (!post.acceptedAnswerID) res.status(422).json('no accepted answer');
+
+      Post.findOne({ soID: post.acceptedAnswerID }, (err2, answer) => {
+        // console.log('>>>>>>>>>>>>> err2', err2, answer);
+        if (err2 || answer === null) {
+          res.status(STATUS_USER_ERROR);
+          return;
+        }
+        res.status(200).json(answer);
       });
     });
 });
+
 server.get('/top-answer/:soID', (req, res) => {
   const { soID } = req.params;
-  Post.find({ parentID: soID })
-    .sort({ score: -1})
-    .exec((err, post) => {
-      if (err) res.status(STATUS_USER_ERROR);
-      else return post;
+  Post.findOne({ soID })
+    .select('acceptedAnswerID')
+    .exec((err1, answerId) => {
+      if (err1 || !answerId) res.status(STATUS_USER_ERROR);
+      else return answerId;
     })
-    .then((scores) => {
-      res.json(scores[0]);
+    .then((value) => {
+      Post.find({ parentID: soID })
+        .sort({ score: -1 })
+        .exec((err, posts) => {
+          if (err) res.status(STATUS_USER_ERROR);
+          console.log('>>>>>>>>>>>>>', posts[0].score, value);
+          for (let i = 0; i < posts.length; i++) {
+            // if (posts[i].score === '5747') console.log(posts[i].soID, value);
+            if (posts[i].soID !== value.acceptedAnswerID) res.json(posts[i]);
+          }
+        });
     });
+  // Post.find({ parentID: soID })
+  //   .sort({ score: -1 })
+  //   .exec((err, posts) => {
+  //     if (err) res.status(STATUS_USER_ERROR);
+
+  //     for (let i = 0; i < scores.length; i++) {
+  //       if posts[i].acceptedAnswerID
+  //     }
+
+  //   })
+  //   .then((scores) => {
+  //     for (let i = 0; i < scores.length; i++) {
+
+  //     }
+  //     res.json(scores[0]);
+  //   });
+});
+server.get('/popular-jquery-questions', (req, res) => {
+  Post.find({ $and: [{ tags: { $in: ['jquery'] } }] });
 });
 module.exports = { server };
