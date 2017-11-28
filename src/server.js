169c11ac20b9cad6ed66d8325a83ed8d7cccbf6b
Cassidy SN Avery <tacolim@gmail.com>
2017-11-27 21:14:19 -0500

    passing 6 of 9 tests; 3 of four routes written

--- src/server.js
+++ src/server.js
@@ -29,4 +29,43 @@ server.get('/accepted-answer/:soID', (req, res) => {
   });
 });
 
+server.get('/top-answer/:soID', (req, res) => {
+  const { soID } = req.params;
+  Post.findOne({ soID }, (err1, foundPost) => {
+    if (err1 || foundPost === null) {
+      res.status(STATUS_USER_ERROR).json(err1);
+      return;
+    }
+    const aid = foundPost.acceptedAnswerID;
+    Post.find()
+      .where({ parentID: soID })
+      .where('soID')
+      .ne(aid)
+      .sort({ score: 'desc' })
+      .exec()
+      .then((err2, posts) => {
+        if (err2 || posts.length === 0 || !posts) {
+          res.status(STATUS_USER_ERROR).json(err2);
+          return;
+        }
+        res.json(posts[0]);
+      });
+  });
+});
+
+server.get('/popular-jquery-questions', (req, res) => {
+  Post.find()
+    .where({ tags: { $in: ['jquery'] } })
+    .where({ $or: [{ score: { $ge: 5000 } }, { 'user.reputation': { $ge: 200000 } }] })
+    .$where({ parentID: null })
+    .exec()
+    .then((err, posts) => {
+      if (err || posts.length === 0 || !posts) {
+        res.status(STATUS_USER_ERROR).json(err);
+        return;
+      }
+      res.json(posts);
+    });
+});
+
 module.exports = { server };
