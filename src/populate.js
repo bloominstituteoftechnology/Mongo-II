f2e164b4887aaec3c6660325b9575b91ed41ce84
Teresa Strout <qapla47@gmail.com>
2017-11-28 17:22:43 -0500

    everything working finally

--- src/populate.js
+++ src/populate.js
@@ -6,15 +6,18 @@ const Post = require('./post.js');
 
 const readPosts = () => {
   // cache posts after reading them once
-  if (!savedPosts) {
-    const contents = fs.readFileSync('posts.json', 'utf8');
-    savedPosts = JSON.parse(contents);
+  if (!savedPosts) { // if there are no savedPosts
+    const contents = fs.readFileSync('posts.json', 'utf8'); // the contents are in the file posts.json, with encoding of utf8 <- most common encoding
+    savedPosts = JSON.parse(contents); // use json to parse the contents of the file
   }
-  return savedPosts;
+  return savedPosts; // gimmie dem posts
 };
 
-const populatePosts = () => {
+  const populatePosts = () => { // now populate the posts
   // TODO: implement this
-};
+    const allPosts = readPosts(); // instantiate the code above
+    const promises = allPosts.map(p => new Post(p).save()); // create a promise that maps over the posts and creates a new Post object and save them to the db
+    return Promise.all(promises); // return the Promise to make it real
+  };
 
 module.exports = { readPosts, populatePosts };
