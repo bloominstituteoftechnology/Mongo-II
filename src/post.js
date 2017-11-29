fe9d6cc6e3b0cc3b99d5b3f9336d230feef27b9c
Christian Franco <christian.franco94@icloud.com>
2017-11-28 18:24:13 -0800

    finished homewokr

--- src/post.js
+++ src/post.js
@@ -10,6 +10,63 @@ mongoose.connect('mongodb://localhost/so-posts', { useMongoClient: true });
 
 const PostSchema = new mongoose.Schema({
   // TODO: write your schema here
+  soID: {
+    type: Number,
+    required: true
+  },
+  parentID: Number,
+  url: {
+    type: String,
+    required: true
+  },
+  title: String,
+  body: {
+    type: String,
+    required: true
+  },
+  score: {
+    type: Number,
+    required: true,
+  },
+  tags: [String],
+  acceptedAnswerID: Number,
+  user: {
+    soUserID: Number,
+    name: String,
+    reputation: Number
+  }
 });
+
+PostSchema.statics.findQuestion = function (id) {
+  return new Promise((resolve, reject) => {
+    this.findOne({ soID: id }, (error, question) => {
+      return error ? resolve(null) : resolve(question);
+    });
+  });
+};
+
+PostSchema.statics.findQuestions = function (options) {
+  return new Promise((resolve, reject) => {
+    this.find(options, (err, questions) => {
+      return err ? reject(err) : resolve(questions);
+    });
+  });
+};
+
+PostSchema.statics.findAnswers = function (questionID) {
+  return new Promise((resolve, reject) => {
+    this.find({ parentID: questionID }, (error, answers) => { return error ? resolve(null) : resolve(answers); });
+  });
+};
+
+PostSchema.statics.getTopAnswer = function (id, acceptedAnswerID) {
+  return new Promise((resolve, reject) => {
+    this.findOne({ soID: { $ne: acceptedAnswerID }, parentID: id })
+    .sort({ score: 'desc' })
+    .exec((error, answer) => {
+      return error ? reject(error) : resolve(answer);
+    });
+  });
+};
 
 module.exports = mongoose.model('Posts', PostSchema);
--- src/server.js
+++ src/server.js
@@ -11,5 +11,74 @@ const server = express();
 server.use(bodyParser.json());
 
 // TODO: write your route handlers here
+server.get('/accepted-answer/:soID', (req, res) => {
+  const { soID } = req.params;
+  const question = Post.findOne({ soID }, (err, post) => {
+    if (err) {
+      res.status(STATUS_USER_ERROR).json({ error: err });
+      return;
+    }
+
+    const { acceptedAnswerID } = post;
+
+    if (!acceptedAnswerID) {
+      res
+        .status(STATUS_USER_ERROR)
+        .json({ error: 'Question does not have an accepted answer' });
+      return;
+    }
+
+    const answer = Post.findOne({ soID: acceptedAnswerID }, (e, ansr) => {
+      if (e) {
+        res.status(STATUS_USER_ERROR).json({ error: e });
+        return;
+      }
+      res.json(ansr);
+    });
+  });
+});
+
+server.get('/top-answer/:soID', async (req, res) => {
+  const { soID } = req.params;
+
+  try {
+    const question = await Post.findQuestion(soID);
+    if (question) {
+      const topAnswer = await Post.getTopAnswer(question.soID, question.acceptedAnswerID);
+      return topAnswer ? res.json(topAnswer) : res.status(STATUS_USER_ERROR).json({ error: 'no answer' });
+    }
+    res.status(STATUS_USER_ERROR).json({ error: 'ID does not match question' });
+  } catch (e) {
+    console.log('catch error: ', e);
+    res.status(STATUS_USER_ERROR).json({ error: e });
+  }
+
+  return;
+});
+
+server.get('/popular-jquery-questions', async (req, res) => {
+  try {
+    const questions = await Post.findQuestions({
+      parentID: null,
+      tags: 'jquery',
+      $or: [{ score: { $gt: 5000 } }, { 'user.reputation': { $gt: 200000 } }],
+    });
+    res.json(questions);
+  } catch (e) {
+    res.status(STATUS_USER_ERROR).json({ error: e });
+  }
+  return;
+});
+
+server.get('/npm-answers', async (req, res) => {
+  try {
+    const questions = await Post.findQuestions({ tags: 'npm' });
+    const answers = await Post.findAnswers({ $in: questions.map(question => question.soID) });
+    res.json(answers);
+  } catch (e) {
+    res.status(STATUS_USER_ERROR).json({ error: e });
+  }
+  return;
+});
 
 module.exports = { server };
