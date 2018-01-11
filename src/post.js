3c183ae656cd5ca32d208f6336eb3b6881933e78
Jason Metten <metten@gmail.com>
2018-01-11 09:43:04 -0600

    All tests pass

--- src/post.js
+++ src/post.js
@@ -9,7 +9,31 @@ mongoose.Promise = Promise;
 mongoose.connect('mongodb://localhost/so-posts', { useMongoClient: true });
 
 const PostSchema = new mongoose.Schema({
-  // TODO: write your schema here
+  soID: {
+    type: Number,
+    required: true,
+  },
+  url: {
+    type: String,
+    required: true,
+  },
+  body: {
+    type: String,
+    required: true,
+  },
+  score: {
+    type: Number,
+    required: true,
+  },
+  parentID: Number,
+  tags: [String],
+  acceptedAnswerID: Number,
+  title: String,
+  user: {
+    soUserID: Number,
+    name: String,
+    reputation: Number,
+  }
 });
 
 module.exports = mongoose.model('Posts', PostSchema);
