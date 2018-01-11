14fb7e5a27dd83a2243109fa022ba21317bccc05
Moises Dobarganes <designerexpert@gmail.com>
2018-01-10 22:06:43 -0500

    Functionality finished, some errors falling through, but it works

--- src/populate.js
+++ src/populate.js
@@ -2,12 +2,13 @@ const fs = require('fs');
 
 let savedPosts = null;
 
+
 const Post = require('./post.js');
 
 const readPosts = () => {
   // cache posts after reading them once
   if (!savedPosts) {
-    const contents = fs.readFileSync('posts.json', 'utf8');
+    const contents = fs.readFileSync('./posts.json', 'utf8');
     savedPosts = JSON.parse(contents);
   }
   return savedPosts;
@@ -15,6 +16,8 @@ const readPosts = () => {
 
 const populatePosts = () => {
   // TODO: implement this
+  const promises = savedPosts.map(postData => new Post(postData).save());
+  return Promise.all(promises);
 };
 
 module.exports = { readPosts, populatePosts };
