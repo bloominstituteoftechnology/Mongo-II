1222be0a9af28b3907997dc2f01dd3eae0d6d7e9
Aaron Wilder <element1028@gmail.com>
2018-01-10 20:15:38 -0800

    working

--- src/server.js
+++ src/server.js
@@ -11,5 +11,45 @@ const server = express();
 server.use(bodyParser.json());
 
 // TODO: write your route handlers here
+server.get('/accepted-answer/:soID', (req, res) => {
+  const { soID } = req.params;
+  Post.findOne({ soID })
+    .then((question) => {
+      const newID = question.acceptedAnswerID;
+      Post.findOne({ soID: newID })
+        .then((answer) => {
+          if (answer === null) {
+            res.status(422).json({ error: 'Dont have an accepted Answer' });
+          } else {
+            res.status(200).json(answer);
+          }
+        })
+        .catch((error) => {
+          res.status(422).json({ error: 'No answer found' });
+        });
+    })
+    .catch((error) => {
+      res.status(422).json({ error: 'No question found' });
+    });
+});
 
+server.get('/top-answer/:soID', (req, res) => {
+  const { soID } = req.params;
+  Post.findOne({ soID })
+    .then((answer) => {
+      const newID = answer.parentID;
+      Post.findOne({ parentID: newID })
+        .sort('-score')
+        .then((answers) => {
+          res.status(200).json(answers);
+        })
+        .catch((err) => {
+          res.status(422).json(err);
+        });
+        
+    })
+    .catch((err) => {
+      res.status(422).json(err);
+    });
+});
 module.exports = { server };
