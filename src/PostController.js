1bfe95e769f77e321541602b31c9471d295dfeff
Jesse Hood <jessenoob@gmail.com>
2018-01-10 20:18:02 -0500

    refactor query to be more efficient

--- src/PostController.js
+++ src/PostController.js
@@ -59,15 +59,13 @@ function getNpmAnswers(req, res) {
     if (!posts.length) throw new Error('No posts found');
     // Take the IDs of all posts that were found
     const IDs = posts.map(post => post.soID);
-    // Create a promise array searching for the answers of each question that was found
-    const answerQuery = IDs.map(id => Posts.find({ parentID: id }));
-    return Promise.all(answerQuery);
+
+    return Posts.find({ parentID: { $in: IDs } });
   }).then((answers) => {
     if (!answers.length) throw new Error('No answers found');
     // Promise.all will return an array of answers for each question
-    // Flatten them into a single array of answers for the API 
-    const flattenAnswers = answers.reduce((acc, next) => acc.concat(next));
-    res.status(200).json(flattenAnswers);
+    // Flatten them into a single array of answers for the API
+    res.status(200).json(answers);
   }).catch((err) => {
     res.status(422).json({ error: err.message });
   });
