de0b33139c2fecad4e622f39fed036ea8c02283d
sarika <sarikaresume@gmail.com>
2018-01-11 11:37:49 -0800

    mongo2

--- src/post.js
+++ src/post.js
@@ -1,11 +1,3 @@
-// Based on the posts given above, and the
-// `posts.json` file if you need more examples, fill in the `PostSchema`. Fields
-//  `soID`, `url`, `body`, and `score` are required; all other fields are optional.
-// Note that `tags` is an array of *strings* and `user` is a *nested object*; make
-// sure you represent their schemas appropriately. Every field that is an id should
-// be represented as a `Number`, not as an `ObjectId`, since these are not MongoDB
-// references, but rather StackOverflow numeric identifiers.
-
 const mongoose = require('mongoose');
 
 // Clear out mongoose's model cache to allow --watch to work for tests:
@@ -17,33 +9,31 @@ mongoose.Promise = Promise;
 mongoose.connect('mongodb://localhost/so-posts', { useMongoClient: true });
 
 const PostSchema = new mongoose.Schema({
-  // TODO: write your schema here 
-    soID: {
-      type: Number,
-      required: true},
-  
-    
-    url: {
-      type: String, 
-      required: true, 
-     },
-
-    body: {
-       type: String, 
-       required: true,
-     },
-
-    score: {
-      type: String, 
-      required: true, 
-    },
-
-    tags: [], 
-    user: [],
-
-      
-    
+  soID: {
+    type: Number,
+    required: true
+  },
+  parentID: Number,
+  url: {
+    type: String,
+    required: true
+  },
+  title: String,
+  body: {
+    type: String,
+    required: true
+  },
+  score: {
+    type: Number,
+    required: true
+  },
+  tags: [String],
+  acceptedAnswerID: Number,
+  user: {
+    soUserID: Number,
+    name: String,
+    reputation: Number,
+  }
 });
 
 module.exports = mongoose.model('Posts', PostSchema);
-
--- src/server.js
+++ src/server.js
@@ -1,16 +1,3 @@
-// In `src/server.js`, add a route handler for `GET /accepted-answer/:soID`. When
-// the client makes a `GET` request to `/accepted-answer/:soID`:
-
-// 1. Find the question with the given `soID` (1 query).
-// 2. Find the accepted answer of the question by using the `acceptedAnswerID`
-//  field (1 query).
-// 3. Send back a JSON response with the single accepted answer post object.
-
-// You should *only* use 2 queries, and no more, for this route. If there's any
-// error, or if there is no accepted answer, report it with an appropriate message
-// and status code.
-
-
 const bodyParser = require('body-parser');
 const express = require('express');
 
@@ -20,91 +7,94 @@ const STATUS_USER_ERROR = 422;
 
 const server = express();
 // to enable parsing of json bodies for post requests
-
 server.use(bodyParser.json());
 
-// TODO: write your route handlers here
-get(function() {
-    return `${this.acceptedAnswerID} ${this.soID}`;
-    {
-      soID: 
-      acceptedAnswerID:,
-      post.find 
+const sendUserError = (err, res) => {
+  res.status(STATUS_USER_ERROR);
+  if (typeof err === 'string') {
+    res.json({ error: err });
+  } else {
+    res.json(err);
+  }
+};
+
+const queryAndThen = (query, res, cb) => {
+  query.exec((err, result) => {
+    if (err) {
+      sendUserError(err, res);
+    } else {
+      cb(result);
+    }
+  });
+};
+
+server.get('/accepted-answer/:soID', (req, res) => {
+  queryAndThen(Post.findOne({ soID: req.params.soID }), res, (post) => {
+    if (!post) {
+      sendUserError("Couldn't find post with given ID", res);
+      return;
     }
 
-  }
-get(function() {
-  return ${this.top-answer:soID} ${this.soID}
-  { 
-    soID: 
-    top-answer:
-    post.find
-  }
-get (function() {
-  return ${this.popular-jquery-questions}
-  {
-    soID: 
-    popular-jquery-questions:
-    post.find
-  }
-  // When the client makes a `GET` request to `/popular-jquery-questions`:'
+    const query = Post.findOne({ soID: post.acceptedAnswerID });
+    queryAndThen(query, res, (answer) => {
+      if (!answer) {
+        sendUserError('No accepted answer', res);
+      } else {
+        res.json(answer);
+      }
+    });
+  });
+});
 
-get (function() {
-  return ${this.npm-answers}
-  //When the client makes a `GET` request to `/npm-answers`:'
-  {
-    soID:
-    npm-answers:
-    post.find 
-  }
-}
-}
+server.get('/top-answer/:soID', (req, res) => {
+  queryAndThen(Post.findOne({ soID: req.params.soID }), res, (post) => {
+    if (!post) {
+      sendUserError("Couldn't find post with given ID", res);
+      return;
+    }
+
+    const query = Post
+      .findOne({
+        soID: { $ne: post.acceptedAnswerID },
+        parentID: post.soID,
+      })
+      .sort({ score: 'desc' });
+
+    queryAndThen(query, res, (answer) => {
+      if (!answer) {
+        sendUserError('No top answer', res);
+      } else {
+        res.json(answer);
+      }
+    });
+  });
+});
+
+server.get('/popular-jquery-questions', (req, res) => {
+  const query = Post.find({
+    parentID: null,
+    tags: 'jquery',
+    $or: [
+      { score: { $gt: 5000 } },
+      { 'user.reputation': { $gt: 200000 } }
+    ]
+  });
+
+  queryAndThen(query, res, posts => res.json(posts));
+});
+
+server.get('/npm-answers', (req, res) => {
+  const query = Post.find({
+    parentID: null,
+    tags: 'npm'
+  });
+
+  queryAndThen(query, res, (posts) => {
+    const answerQuery = Post.find({
+      parentID: { $in: posts.map(p => p.soID) }
+    });
+    queryAndThen(answerQuery, res, answers => res.json(answers));
+  });
+});
+
 module.exports = { server };
-
-// ### `GET /accepted-answer/:soID`
-// In `src/server.js`, add a route handler for `GET /accepted-answer/:soID`. When
-// the client makes a `GET` request to `/accepted-answer/:soID`:
-
-// 1. Find the question with the given `soID` (1 query).
-// 2. Find the accepted answer of the question by using the `acceptedAnswerID`
- //  field (1 query).
-// 3. Send back a JSON response with the single accepted answer post object.
-
-// You should *only* use 2 queries, and no more, for this route. If there's any
-// error, or if there is no accepted answer, report it with an appropriate message
-// and status code.
-
-// ### `GET /top-answer/:soID`
-// When the client makes a `GET` request to `/top-answer/:soID`:
-
-// 1. Find the question with the given `soID` (1 query).
-// 2. Find the answer of the given question that has the *highest* score and *is
-//   not the accepted answer* (1 query).
-// 3. Send back a JSON response with the single top answer post object.
-
-//You should *only* use 2 queries, and no more, for this route. If there's any
-//error, or if there is no top answer, report it with an appropriate message
-//and status code.
-
-// ### `GET /popular-jquery-questions`
-// When the client makes a `GET` request to `/popular-jquery-questions`:
-
-// 1. Find all question posts that are tagged with `jquery` and *either* have
-//   a score greater than 5000, or are posted by a user with reputation greater
-//   than 200,000 (1 query).
-//2. Send back a JSON response with an array of popular jquery questions.
-
-// You should *only* use 1 query, and no more, for this route. If there's any
-// error, report it with an appropriate message and status code.
-
-//### `GET /npm-answers`
-//When the client makes a `GET` request to `/npm-answers`:
-
-//1. Find all question posts that are tagged with `npm` (1 query).
-//2. Find all answers to all questions above (1 query).
-//3. Send back a JSON response with an array of answers to npm questions.
-
-//You should *only* use 2 queries, and no more, for this route. If there's any
-//error, report it with an appropriate message and status code.
-
-
