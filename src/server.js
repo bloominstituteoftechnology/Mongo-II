e2efe68c24f155bf4ce8ae2acd60ba6be4716849
Adam Fetters <adamfetterscoder@gmail.com>
2018-01-11 09:03:18 -0500

    All but last query done

--- src/server.js
+++ src/server.js
@@ -19,13 +19,42 @@ server.get('/accepted-answer/:soID', (req, res) => {
     })
     .then((post) => {
       if (post === 'undefined') {
-        res.status(404).json({ error: 'ID Not Found' });
+        res.status(STATUS_USER_ERROR).json({ error: 'ID Not Found' });
       } else {
         res.status(200).json(post);
       }
     })
     .catch((err) => {
       res.status(500).json({ error: 'Error getting data' });
+    });
+});
+
+server.get('/top-answer/:soID', (req, res) => {
+  const id = req.params.soID;
+  Post.find({ parentID: id }, { score: 1 })
+    .sort({ score: -1 })
+    .then((post) => {
+      return Post.findOne({ score: { $ne: post[0].score } });
+    })
+    .then((post) => {
+      res.status(200).json(post);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: 'Error getting data' });
+    });
+});
+
+server.get('/popular-jquery-questions', (req, res) => {
+  Post.find({ tags: { $in: ['jquery'] } })
+    .where('score')
+    .gt(5000)
+    .where('user.reputation')
+    .gt(200000)
+    .then((post) => {
+      res.status(200).json(post);
+    })
+    .catch((error) => {
+      res.status(500).json({ error: "Couldn't retrieve the data" });
     });
 });
 
