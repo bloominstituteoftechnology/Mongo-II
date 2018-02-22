639c33fe7a078aa368515ae860d0d6d78c6db64e
Calum Groover <c.groover@outlook.com>
2018-02-22 15:53:36 -0500

    post schema completed

--- src/post.js
+++ src/post.js
@@ -9,7 +9,48 @@ mongoose.Promise = Promise;
 mongoose.connect('mongodb://localhost/so-posts', { useMongoClient: true });
 
 const PostSchema = new mongoose.Schema({
-  // TODO: write your schema here
+  soID: {
+    type: Number,
+    required: true,
+  },
+  parentID: {
+    type: Number,
+  },
+  url: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,    
+  },
+  body: {
+    type: String,
+    required: true,
+  },
+  score: {
+    type: Number,
+    required: true,
+  },
+  tags: {
+    type: Array,
+  },
+  acceptedAnswerID: {
+    type: Number
+  },
+  user: {
+    soUserID: {
+      type: Number,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    reputation: {
+      type: Number,
+      required: true,
+    }
+  }
 });
 
 module.exports = mongoose.model('Posts', PostSchema);
