0f020ea879cb43960450de143643f041c8edd9e1
Kevin <16568166+sirkevinc@users.noreply.github.com>
2018-03-14 16:51:11 -0400

    Worked on Populate Posts and began creating routes
    Trying to get populate posts to work correctly
    
    Created acceptedanswer route

--- src/populate.js
+++ src/populate.js
@@ -5,6 +5,26 @@ let savedPosts = null;
 
 const Post = require('./post.js');
 
+const populatePosts = () => {
+  readPosts();
+  let tempArr = savedPosts;
+  const posts = new Post ({ tempArr });
+  posts.save()
+    .then(posts => {
+      res.status(200).json(posts)
+    })
+    .catch(err => {
+      res.status(500).json({error: "err"});
+    })
+  Promise.all(posts)
+    .then(posts => {
+      res.json({Success: "Nice!"});
+    })
+    .catch(err => {
+      res.json({Error: err});
+    })
+};
+
 const readPosts = () => {
   // cache posts after reading them once
   if (!savedPosts) {
--- src/server.js
+++ src/server.js
@@ -11,5 +11,33 @@ const server = express();
 server.use(bodyParser.json());
 
 // TODO: write your route handlers here
+server.get('/accepted-answer/:soID', (req, res) => {
+  let answerID = null;
+  const { soID } = req.params;
+  Post.find({ "soID": soID })
+    .then(post => {
+      answerID = post.acceptedAnswerID
+      Post.find({ "answerID": answerID })
+        .then(post => {
+          res.status(200).json({AcceptedAnswer: post})
+        })
+        .catch(err => {
+          res.status(500).json(err);
+        })
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    })
+})
+
+server.get('/', (req, res) => {
+  Post.find()
+    .then(posts => {
+      res.status(200).json(posts)
+    })
+    .catch(err => {
+      res.status(500).json({ Error: err });
+    })
+})
 
 module.exports = { server };
