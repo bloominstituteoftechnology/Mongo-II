c3146aceff1a3a6d41ca7a51f3718461ed8854f0
Terrie B. Kim <terrieb.kim@gmail.com>
2018-03-14 16:15:46 -0700

    POPULATE complete, GET accepted answer by soID complete

--- src/server.js
+++ src/server.js
@@ -3,7 +3,11 @@ const express = require('express');
 
 const Post = require('./post.js');
 
+
+const STATUS_OK = 200;
+const STATUS_NOT_FOUND = 404;
 const STATUS_USER_ERROR = 422;
+const STATUS_SERVER_ERROR = 500;
 
 const server = express();
 // to enable parsing of json bodies for post requests
@@ -11,5 +15,28 @@ const server = express();
 server.use(bodyParser.json());
 
 // TODO: write your route handlers here
+server.get('/accepted-answer/:soID', (req, res) => {
+  const {
+    soID
+   } = req.params;
+
+  Post.findOne({ soID })
+    .then(post => {
+      Post.findOne({ soID: post.acceptedAnswerID })
+        .then(answer => {
+          if(answer) {
+            res.status(200).json(answer);
+          } else {
+            res.status(404).json({ message: "The post with specified ID does not exisit." });
+          }
+        })
+        .catch(error => {
+          res.status(500).json({ error: "The information could not be retrieved." });
+        });
+    })
+    .catch(error => {
+      res.status(500).json({ error: "The information could not be retrieved." });
+    });
+});
 
 module.exports = { server };
