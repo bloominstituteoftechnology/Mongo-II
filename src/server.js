268b509b085951694330aaa949361dcadf1b8977
Velsu <riftmage50@gmail.com>
2018-05-10 18:16:39 +0200

    Added accepted-answer endpoint

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
-const bodyParser = require('body-parser');
-const express = require('express');
+const bodyParser = require("body-parser");
+const express = require("express");
 
-const Post = require('./post.js');
+const Post = require("./post.js");
 
 const STATUS_USER_ERROR = 422;
 
@@ -11,5 +11,30 @@ const server = express();
 server.use(bodyParser.json());
 
 // TODO: write your route handlers here
+
+server.get("/accepted-answer/:soID", (req, res, next) => {
+  const { soID } = req.params;
+  Post.findOne({ soID })
+    .then(post => {
+      Post.findOne({ soID: post.acceptedAnswerID })
+        .then(answer => {
+          if (answer) {
+            res.status(200).json(answer);
+          } else {
+            res
+              .status(404)
+              .json({ message: "There is no answer with such ID" });
+          }
+        })
+        .catch(error => {
+          res
+            .status(500)
+            .json({ error: "The information could not be obtained" });
+        });
+    })
+    .catch(error => {
+      res.status(500).json({ error: "The information could not be obtained" });
+    });
+});
 
 module.exports = { server };
